3fae20b6f7f54df36b10c092622f7bf02754aaf3
Zachsopone <apdujanexequiel@gmail.com>
2025-11-28 17:28:24 +0800

    fix protected route

--- Ebingo/src/route/ProtectedRoute.jsx
+++ Ebingo/src/route/ProtectedRoute.jsx
@@ -2,7 +2,8 @@ import { Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import PropTypes from "prop-types";
 
-const ProtectedRoute = ({ children, allowedRoles, allowClosed = true }) => {
+// allowClosed = true
+const ProtectedRoute = ({ children, allowedRoles }) => {
   //console.log("Cookies after login:", Cookies.get());
   const token = Cookies.get("accessToken");
   if (!token) return <Navigate to="/" replace />;
@@ -15,16 +16,16 @@ const ProtectedRoute = ({ children, allowedRoles, allowClosed = true }) => {
     userRole = (payload.role || "").toLowerCase();
 
     // Cashier/Guard working hours check
-    if (!allowClosed && ["cashier", "guard"].includes(userRole)) {
-      const now = new Date();
-      const currentMinutes = now.getHours() * 60 + now.getMinutes();
-      const startMinutes = 9 * 60;        // 9:00 AM
-      const endMinutes = 18 * 60 + 5;
+    // if (!allowClosed && ["cashier", "guard"].includes(userRole)) {
+    //   const now = new Date();
+    //   const currentMinutes = now.getHours() * 60 + now.getMinutes();
+    //   const startMinutes = 9 * 60;        // 9:00 AM
+    //   const endMinutes = 18 * 60 + 5;
 
-      if (currentMinutes < startMinutes || currentMinutes > endMinutes) {
-        return <Navigate to="/closed" replace />;
-      }
-    }
+    //   if (currentMinutes < startMinutes || currentMinutes > endMinutes) {
+    //     return <Navigate to="/closed" replace />;
+    //   }
+    // }
   } catch (err) {
     console.error("Invalid token:", err);
     return <Navigate to="/" replace />;
