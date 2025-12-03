330ccd437d5272d91139bd614de4528ed0be7c82
Zachsopone <apdujanexequiel@gmail.com>
2025-12-03 22:16:41 +0800

    try

--- backend/routes/branches.route.js
+++ backend/routes/branches.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getBranches, updateBranch, updateBranchTime, addBranch, deleteBranch, getBranchById } from "../controllers/branches.controller.js";
+import { getBranches, updateBranch, updateBranchTime, addBranch, deleteBranch, getBranchById, getBranchesFiltered, } from "../controllers/branches.controller.js";
+import verifyUser from "../middlewares/auth.middleware.js";
 
 
 const router = express.Router();
 
 router.get("/", getBranches);     // GET /branches → list all
+router.post("/", verifyUser(["superadmin","kaizen","cashier","guard"]), getBranchesFiltered);
 router.post("/add", addBranch);         // POST /branches/add → add new
 router.put("/:id", updateBranch);       // PUT /branches/:id → update
 router.put("/:id/time", updateBranchTime);  // updates opening_time & closing_time only
