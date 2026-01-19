8812d341b882ab3be75b5627e6f17033196f134e
Zachsopone <apdujanexequiel@gmail.com>
2026-01-19 23:58:46 +0800

    brancname in branches & users page

--- Ebingo/src/pages/Cashier.jsx
+++ Ebingo/src/pages/Cashier.jsx
@@ -37,7 +37,7 @@ const Cashier = () => {
 
   return (
     <div className="w-full h-screen">
-      <Header fixedBranchId={branchId}/>
+      <Header/>
       <Navigation triggerRefetch={() => setRefetchKey((prev) => prev + 1)} />
       <Members fixedBranchId={branchId} refetchKey={refetchKey} />
 
